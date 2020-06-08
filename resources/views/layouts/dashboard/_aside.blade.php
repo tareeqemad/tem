58d9204c6802692494e12be7e016483b1f824c97
tareq elbawab <telbawab@gedco.com>
2020-06-08 12:46:13 +0200

    first commit

---
+++ resources/views/layouts/dashboard/_aside.blade.php
@@ -0,0 +1,38 @@
+<div class="vertical-menu">
+
+    <div class="h-100">
+
+        <div class="user-wid text-center py-4">
+            <div class="user-img">
+                <img src="{{ asset('assets/images/users/avatar-2.jpg') }}" alt="" class="avatar-md mx-auto rounded-circle">
+            </div>
+
+            <div class="mt-3">
+
+                <a href="#" class="text-dark font-weight-medium font-size-16">Patrick Becker</a>
+                <p class="text-body mt-1 mb-0 font-size-13">UI/UX Designer</p>
+
+            </div>
+        </div>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Menu</li>
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="mdi mdi-airplay"></i><span class="badge badge-pill badge-info float-right">2</span>
+                        <span>Dashboard</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="index.html">Dashboard 1</a></li>
+                        <li><a href="index-2.html">Dashboard 2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
