48c2c6e58d04576d7001676eea1cc0c85499689f
Tareq Elbawab <tareqelbawab94@gmail.com>
2020-06-10 21:37:40 +0300

    some organize code and make tb

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
@@ -37,9 +38,46 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $data['customer_name'] = $request->customer_name;
+        $data['customer_email'] = $request->customer_email;
+        $data['customer_mobile'] = $request->customer_mobile;
+        $data['company_name'] = $request->company_name;
+        $data['invoice_number'] = $request->invoice_number;
+        $data['invoice_date'] = $request->invoice_date;
+        $data['sub_total'] = $request->sub_total;
+        $data['discount_type'] = $request->discount_type;
+        $data['discount_value'] = $request->discount_value;
+        $data['vat_value'] = $request->vat_value;
+        $data['shipping'] = $request->shipping;
+        $data['total_due'] = $request->total_due;
 
+        $invoice = Invoice::create($data);
+
+        $details_list = [];
+        for ($i = 0; $i < count($request->product_name); $i++) {
+            $details_list[$i]['product_name'] = $request->product_name[$i];
+            $details_list[$i]['unit'] = $request->unit[$i];
+            $details_list[$i]['quantity'] = $request->quantity[$i];
+            $details_list[$i]['unit_price'] = $request->unit_price[$i];
+            $details_list[$i]['row_sub_total'] = $request->row_sub_total[$i];
+        }
+
+        $details = $invoice->details()->createMany($details_list);
+
+        if ($details) {
+            return redirect()->back()->with([
+                'message' => __('Frontend/frontend.created_successfully'),
+                'alert-type' => 'success'
+            ]);
+        } else {
+            return redirect()->back()->with([
+                'message' => __('Frontend/frontend.created_failed'),
+                'alert-type' => 'danger'
+            ]);
+        }
+
+
+    }
     /**
      * Display the specified resource.
      *
